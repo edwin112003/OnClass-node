228f59aacf34414799ce82a3d62224c12a768d49
Andrelyn12035 <kevo1803wow@gmail.com>
2020-12-27 20:07:36 -0600

    Prueba de tomar nota

---
+++ src/public/js/notas.js
@@ -0,0 +1,19 @@
+    $('#summernote').summernote({
+        height: 300,                 // set editor height
+        minHeight: null,             // set minimum height of editor
+        maxHeight: null,             // set maximum height of editor
+        focus: true,                  // set focus to editable area after initializing summernote
+        toolbar: [
+            ['font', ['bold', 'underline', 'clear']],
+            ['color', ['color']],
+            ['para', ['ul', 'ol', 'paragraph']],
+            ['table', ['table']],
+            ['insert', ['link', 'picture']],
+            ['view', ['codeview', 'help']],
+          ]
+      });
+
+function guardar(){
+    var text = $('#summernote').summernote('code');
+    console.log(text);
+}
